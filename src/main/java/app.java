5891e4758a5a386f568e3b0d36a1a3603e2f07c7
Jeison Barbosa <jdbarbosa95@gmail.com>
2020-07-15 10:14:06 -0500

    First commit

---
+++ src/main/java/app.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+public class app {
+
+    public static void main(String[] arg){
+
+        Conexion connection = new Conexion();
+
+        try(Connection cnx = connection.get_connection()) {
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
