5a248cfdb7befeaba02ff9f9b2fd2832f55ee265
Jeison Barbosa <jdbarbosa95@gmail.com>
2020-07-15 15:39:31 -0500

    formateo de codigo

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,13 +2,13 @@ import java.sql.Connection;
 
 public class app {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
 
         Conexion connection = new Conexion();
 
-        try(Connection cnx = connection.get_connection()) {
+        try (Connection cnx = connection.get_connection()) {
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
